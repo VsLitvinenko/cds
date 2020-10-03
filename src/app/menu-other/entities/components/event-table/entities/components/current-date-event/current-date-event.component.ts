7c85ce9ba99f2d4a745317e6de5d0ef942fcd188
vlitv-rgu <alorsl@live.com>
2020-10-03 23:25:15 +0300

    feat(app) current date event get func & styles

--- src/app/menu-other/entities/components/event-table/entities/components/current-date-event/current-date-event.component.ts
+++ src/app/menu-other/entities/components/event-table/entities/components/current-date-event/current-date-event.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {EventTableService} from '../../services/event-table.service';
+import {EventObjectInterface} from '../../interfaces/event-object.interface';
 
 @Component({
   selector: 'app-current-date-event',
@@ -8,11 +10,31 @@ import { Component, OnInit } from '@angular/core';
 export class CurrentDateEventComponent implements OnInit {
   public currentDate: Date;
   public subTitle: string;
+  public eventObjects: EventObjectInterface[];
 
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  constructor(private _eventTableService: EventTableService) { }
 
   ngOnInit() {
-    this.subTitle = `Мероприятия ${this.currentDate.getDate()}.${this.currentDate.getMonth() + 1}.${this.currentDate.getFullYear()}`;
+    const day = this.currentDate.getDate();
+    const month = this.currentDate.getMonth();
+    const year = this.currentDate.getFullYear();
+    this.subTitle = `Мероприятия ${day > 9 ? day : `0${day}`}.${month > 8 ? month + 1 : `0${month + 1}`}.${year}`;
+
+    this._eventTableService.eventObjects$.subscribe( (items: EventObjectInterface[]) => {
+      this.eventObjects = items;
+    });
+    this._eventTableService.getEventObjects(new Date());
+  }
+
+  public getStartTimeFromEventObject(e: EventObjectInterface): string {
+    const hours = e.currentIEvent.startTime.getHours();
+    const minutes = e.currentIEvent.startTime.getMinutes();
+    return `${hours > 9 ? hours : `0${hours}`}:${minutes > 9 ? minutes : `0${minutes}`}`;
+  }
+
+  public getEndTimeFromEventObject(e: EventObjectInterface): string {
+    return `${e.currentIEvent.endTime.getHours()}:${e.currentIEvent.endTime.getMinutes()}`;
   }
 
 }
--- src/app/menu-other/entities/components/event-table/entities/services/event-table.service.ts
+++ src/app/menu-other/entities/components/event-table/entities/services/event-table.service.ts
@@ -15,7 +15,7 @@ export class EventTableService {
     constructor() {
     }
     public getIEvents(): void {
-        const events = [
+        const IEvents = [
             {
                 title: 'Мероприятие 1',
                 startTime: new Date(2020, 9, 4),
@@ -35,6 +35,38 @@ export class EventTableService {
                 allDay: false
             },
         ];
-        this._iEvents$$.next(events);
+        this._iEvents$$.next(IEvents);
+    }
+
+    public getEventObjects(date: Date): void {
+        const eventObjects = [
+            {
+                id: 1,
+                currentIEvent: {
+                    title: 'Мероприятие 1',
+                    startTime: new Date(2020, 9, 4),
+                    endTime: new Date(2020, 9, 4, 10, 25),
+                    allDay: false,
+                },
+                location: '1214',
+                organizer: 'Slavik',
+                phone: '89165940788',
+                notes: 'blablabla',
+            },
+            {
+                id: 2,
+                currentIEvent: {
+                    title: 'Мероприятие 2',
+                    startTime: new Date(2020, 9, 4, 12, 30),
+                    endTime: new Date(2020, 9, 4, 18, 55),
+                    allDay: false
+                },
+                location: '818',
+                organizer: 'Polina',
+                phone: '89123456766',
+                notes: 'dfgdfgjfdgndfgdfgl',
+            },
+        ];
+        this._eventObjects$$.next(eventObjects);
     }
 }
