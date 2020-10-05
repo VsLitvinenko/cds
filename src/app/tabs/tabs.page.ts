d0a80e62a6d6460166faebbc9117dd5a549016aa
Litvinenko <alorsl@live.com>
2020-10-05 14:58:56 +0300

    fix(app): hide tabs when keyboard is open

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -7,11 +7,25 @@ import {TabService} from './entities/services/tab.service';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  public isTabsVisible: boolean = true;
+  private _defaultHeight: number;
 
   // tslint:disable-next-line:variable-name
-  constructor(private _tabService: TabService) {}
+  constructor(private _tabService: TabService) {
+    this._defaultHeight = window.innerHeight;
+  }
 
   public changeTab(newTab: string): void {
     this._tabService.changeCurrentTab(newTab);
+  }
+
+  public onResize(event) {
+    console.log(event.target.innerHeight);
+    if (event.target.innerHeight < this._defaultHeight) {
+      this.isTabsVisible = false;
+    }
+    else {
+      this.isTabsVisible = true;
+    }
   }
 }
