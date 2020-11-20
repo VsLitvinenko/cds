4e5bd0299ed39cb4145af99559ffe2de70a2e5d2
vlitv-rgu <alorsl@live.com>
2020-11-20 16:03:29 +0300

    feat(app) back-button

--- src/app/common/components/cds-nav/cds-nav.component.ts
+++ src/app/common/components/cds-nav/cds-nav.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {TabService} from '../../../tabs/entities/services/tab.service';
-import {IonNav} from '@ionic/angular';
+import {IonNav, Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-cds-nav',
@@ -12,19 +12,28 @@ export class CdsNavComponent implements OnInit {
   @ViewChild('nav', {static: false}) nav: IonNav;
 
   // tslint:disable-next-line:variable-name
-  constructor(private _tabService: TabService) { }
+  constructor(private _tabService: TabService, private _platform: Platform) { }
 
   ngOnInit() {
+    this._platform.backButton.subscribe( () => {
+      this.nav?.canGoBack().then((allow: boolean) => {
+        if (allow) {
+          this.nav.pop().then();
+        }
+        else {
+          navigator['app'].exitApp();
+        }
+      });
+    });
+
     this._tabService.currentTab$.subscribe((newTab: string) => {
       this.nav?.canGoBack().then((allow: boolean) => {
         if (allow && newTab === 'back') {
           this.nav.pop().then();
-        }
-        else if (allow) {
+        } else if (allow) {
           this.nav.popToRoot().then();
         }
       });
     });
   }
-
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,6 +1,5 @@
 import {Component, } from '@angular/core';
 import {TabService} from './entities/services/tab.service';
-import {Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -14,16 +13,8 @@ export class TabsPage {
   private _defaultHeight: number;
 
   // tslint:disable:variable-name
-  constructor(
-      private _tabService: TabService,
-      private _platform: Platform,
-  )
-  {
+  constructor(private _tabService: TabService) {
     this._defaultHeight = window.innerHeight;
-
-    this._platform.backButton.subscribe( () => {
-      this._tabService.changeCurrentTab('back');
-    });
   }
 
   public changeTab(newTab: string): void {
